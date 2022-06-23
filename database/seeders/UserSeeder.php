f038706c4c128aa84669c3639683a326c100f706
hiroki-yod <hiroki.yod@gmail.com>
2022-06-23 16:34:38 +0900

    create diary table & use seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => '筏千丸',
+                'email' => Str::random(10).'@gmail.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => '松本隼也',
+                'email' => Str::random(10).'@gmail.com',
+                'password' => Hash::make('password'),   
+            ]
+        ]);
+    }
+}
